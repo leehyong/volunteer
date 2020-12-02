3513d315ec3fe0b5833a596c1d140027a85e5f19
leehyong <947903484@qq.com>
2020-12-02 20:19:43 +0800

    sql & dependencies

--- sql/v1.sql
+++ sql/v1.sql
@@ -11,7 +11,7 @@ CREATE TABLE if not exists `user` (
   `country_code` char(4) NOT NULL comment '国家号',
   `role` varchar(10) NOT NULL comment '角色：admin，管理员；super，超管；manager，经理；normal，普通',
   `sex` char(1) NOT NULL comment '性别：男，女',
-  `is_delete` tinyint not null default 0
+  `is_delete` tinyint not null default 0,
   PRIMARY KEY (`id`),
   KEY `ix_parent_id_depth_role` (`parent_id`, `depth`, `role`),
   KEY `ix_ancestor_id_role` (`ancestor_id`, `role`)
@@ -21,14 +21,14 @@ CREATE TABLE if not exists  `third_party_user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `create_time` TIMESTAMP NOT NULL default current_timestamp,
   `update_time` TIMESTAMP NOT NULL default current_timestamp,
-  `user_id` int default NOT NULL comment '账户id',
+  `user_id` int NOT NULL comment '账户id',
   `account` varchar(64) NOT NULL comment '账户',
-  `token` varchar(256) NOT NULL default '' comment '第三方token',
+  `token` varchar(256) NOT NULL comment '第三方token',
   `type` varchar(16) NOT NULL comment '第三方登陆类型：wechat，微信；alipay，支付宝；taobao，淘宝；',
   `avatar` varchar(2048) NOT NULL default '' comment '第三方用户的头像链接',
-  `is_delete` tinyint not null default 0
-  PRIMARY KEY (`id`)
-  KEY `ix_account_type_is_delete` (`account`, `type`, `is_delete`)
+  `is_delete` tinyint not null default 0,
+  PRIMARY KEY (`id`),
+  KEY `ix_account_type_is_delete` (`account`, `type`, `is_delete`),
   CONSTRAINT `third_party_user_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='第三方用户登陆表';
 
@@ -45,7 +45,7 @@ CREATE TABLE if not exists `activity` (
   `type`  char(16) NOT NULL comment '类型:gym, 体育；meeting， 会议；',
   `apply_url`  varchar(2048) NOT NULL comment '报名链接',
   `text`  text NOT NULL comment '内容',
-  `is_delete` tinyint DEFAULT NULL '是否删除'
+  `is_delete` tinyint not NULL default 0 comment '是否删除',
   PRIMARY KEY (`id`),
   KEY `ix_start_time_end_time_type` (`start_time`, `end_time`, `type`),
   CONSTRAINT `avtivity_fk1` FOREIGN KEY (`last_editor_id`) REFERENCES `user` (`id`)
@@ -58,7 +58,7 @@ CREATE TABLE if not exists  `apply` (
   `update_time` TIMESTAMP NOT NULL default current_timestamp,
   `user_id` int NOT NULL comment '创建者id',
   `activity_id` int NOT NULL comment '创建者id',
-  `is_delete` tinyint DEFAULT NULL '是否删除'
+  `is_delete` tinyint not NULL DEFAULT 0 comment '是否删除',
   PRIMARY KEY (`id`),
   CONSTRAINT `apply_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `apply_fk2` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`id`)
