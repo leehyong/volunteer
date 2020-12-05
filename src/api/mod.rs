409fa737b656e327f36a40875d3b6057f749a550
leehyong <947903484@qq.com>
2020-12-05 17:59:59 +0800

    fix middleware

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -7,19 +7,36 @@ use crate::state::AppState;
 use tide::{with_state, Server};
 
 pub fn api_route(app: &mut Server<AppState>) {
-    // 自动在路径前面加上 '/'
+    api_auth_route(app);
+    api_no_auth_route(app);
+    admin_auth_route(app);
+}
+
+fn api_auth_route(app: &mut Server<AppState>) {
+    // 这里加入需要鉴权和认证的的URL
     app.at(API_PATH)
-        .at("activity")
-        .get(ActivityApi::list)
-        .post(ActivityApi::new)
-        .at(":id")
-        .get(ActivityApi::detail);
+        .with(jwt_auth_middleware);
+    // .at("activity")
+    // .get(ActivityApi::list)
+    // .post(ActivityApi::new)
+    // .at(":id")
+    // .get(ActivityApi::detail);
+}
+
+
+fn api_no_auth_route(app: &mut Server<AppState>) {
+    // 这里加入不需要鉴权和认证的的URL
     app.at(API_PATH)
+        // 自动在路径前面加上 '/'
         .at("apply")
-        .with(jwt_auth_middleware)
         .post(|_| async { Ok("Hello, world!") });
+}
+
+fn admin_auth_route(app: &mut Server<AppState>) {
+    // 这里加入管理后台需要鉴权和认证的的URL
     app.at(ADMIN_PATH)
         .with(jwt_auth_middleware)
+        // 自动在路径前面加上 '/'
         .at("users")
         .get(|_| async { Ok("all users") });
 }
