02a93bebac3b46d08c4c98acc3002f4a1f3cc515
leehyong <947903484@qq.com>
2020-12-06 17:43:18 +0800

    use UTF8MB4

--- sql/v1.sql
+++ sql/v1.sql
@@ -7,9 +7,9 @@ CREATE TABLE if not exists `user` (
   `name` varchar(64) NOT NULL comment '名字',
   `parent_id` int default NULL comment '父账户id',
   `ancestor_id` int default NULL comment '祖先id',
-  `depth`     tinyint NOT NULL comment '账号的层级深度',
+  `depth`     tinyint NOT NULL default 0 comment '账号的层级深度',
   `mobile` varchar(20) NOT NULL comment '手机号',
-  `lang` varchar(10) NOT NULL comment '语言',
+  `lang` varchar(20) NOT NULL comment '语言',
   `country_code` char(4) NOT NULL comment '国家号',
   `role` varchar(10) NOT NULL comment '角色：admin，管理员；super，超管；manager，经理；normal，普通',
   `sex` char(1) NOT NULL comment '性别：男，女',
@@ -17,7 +17,7 @@ CREATE TABLE if not exists `user` (
   PRIMARY KEY (`id`),
   KEY `ix_parent_id_depth_role` (`parent_id`, `depth`, `role`),
   KEY `ix_ancestor_id_role` (`ancestor_id`, `role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='用户表';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 comment='用户表';
 
 CREATE TABLE if not exists  `third_party_user` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -32,7 +32,7 @@ CREATE TABLE if not exists  `third_party_user` (
   PRIMARY KEY (`id`),
   KEY `ix_account_source_is_delete` (`account`, `source`, `is_delete`),
   CONSTRAINT `third_party_user_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='第三方用户登陆表';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 comment='第三方用户登陆表';
 
 
 CREATE TABLE if not exists `activity` (
@@ -49,9 +49,9 @@ CREATE TABLE if not exists `activity` (
   `content`  text NOT NULL comment '内容',
   `is_delete` tinyint not NULL default 0 comment '是否删除',
   PRIMARY KEY (`id`),
-  KEY `ix_start_time_end_time_activity_type` (`start_time`, `end_time`, `activity_type`),
+  KEY `ix_end_time_start_time_activity_type` (`end_time`,`start_time`,`activity_type`),
   CONSTRAINT `avtivity_fk1` FOREIGN KEY (`last_editor_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='志愿者活动表';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 comment='志愿者活动表';
 
 
 CREATE TABLE if not exists  `apply` (
@@ -64,5 +64,12 @@ CREATE TABLE if not exists  `apply` (
   PRIMARY KEY (`id`),
   CONSTRAINT `apply_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `apply_fk2` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='志愿者报名表';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 comment='志愿者报名表';
 
+
+-- test data
+insert into activity(creator_id, last_editor_id, subject, activity_type, apply_url, content, start_time, end_time)
+values (1, 1, '金山演唱会', 'concert', 'https://1234.a.b/apply1', '来了来啦， 十大；做过路过不要错过', '20201201', '20210221'),
+         (1, 1, '华山论剑大会', 'kongfu', 'https://1234.a.b/apply2', '来了来啦， 机会来了；做过路过不要错过', '20201201', '20210221'),
+         (1, 1, '医学论坛', 'meeting', 'https://metting.a.b/apply3', '来了来啦， 机会来了；做过路过不要错过', '20201201', '20210221'),
+         (1, 1, '光明顶围剿大会', 'kongfu', 'https://kongfu.a.b/apply4', '来了来啦， 武林至尊，宝刀屠龙，；号令天下， 唯我独尊', '20201201', '20210221');
